18194e84aa30c50943dbc4f93c373328ef4dd555
Adam <lottadam@umich.edu>
2019-08-28 19:54:08 -0400

    made initial API call and loaded content to homepage

---
+++ BeerGenius/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BeerGenius.Models;
+using System.Net.Http;
+
+namespace BeerGenius.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://sandbox-api.brewerydb.com/v2/");
+
+            var response = await client.GetAsync($"styles/?key=7ff275d01954f19419c312477a03e672");
+            var content = await response.Content.ReadAsAsync<StyleRequest>();
+            return View(content);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
