2fd291a09e9bd74ad2dbbeb209e9a7fc6382f3d7
torallify <torallify@gmail.com>
2019-08-28 21:01:24 -0400

    add db with connection string

---
+++ BeerGenius/Data/BeerGeniusDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerGenius.Data
+{
+    public class BeerGeniusDbContext : DbContext
+    {
+        public BeerGeniusDbContext(DbContextOptions<BeerGeniusDbContext> options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
