6646018b2aa528d85c229c71fa26bd52cd7e9e09
MadisonMott <Madison.Mott@ymail.com>
2019-08-29 20:25:09 -0400

    created table contents for FlavorProfile

--- BeerGenius/Data/BeerGeniusDbContext.cs
+++ BeerGenius/Data/BeerGeniusDbContext.cs
@@ -18,10 +18,10 @@ namespace BeerGenius.Data
             {
                 p.HasKey(_ => _.Id);
                 p.HasData(
-                 new FlavorProfile() { Id = 66, Color = 0, Aroma = 0, Crisp = 0, Hop = 0, Malt = 0, Fruity = 0, Sour = 0, ABV = 0, Roasty = 0, Sweetness = 0 },
-                 new FlavorProfile() { Id = 2, Color = 0, Aroma = 0, Crisp = 0, Hop = 0, Malt = 0, Fruity = 0, Sour = 0, ABV = 0, Roasty = 0, Sweetness = 0 },
-                 new FlavorProfile() { Id = 3, Color = 0, Aroma = 0, Crisp = 0, Hop = 0, Malt = 0, Fruity = 0, Sour = 0, ABV = 0, Roasty = 0, Sweetness = 0 },
-                 new FlavorProfile() { Id = 4, Color = 0, Aroma = 0, Crisp = 0, Hop = 0, Malt = 0, Fruity = 0, Sour = 0, ABV = 0, Roasty = 0, Sweetness = 0 }
+                 new FlavorProfile() { Id = 66, Color = 2, Aroma = 1, Crisp = 7, Hop = 1, Malt = 1, Fruity = 10, Sour = 6, ABV = 6, Roasty = 0, Sweetness = 6 },
+                 new FlavorProfile() { Id = 43, Color = 10, Aroma = 10, Crisp = 0, Hop = 5, Malt = 10, Fruity = 1, Sour = 0, ABV = 8, Roasty = 8, Sweetness = 4 },
+                 new FlavorProfile() { Id = 75, Color = 1, Aroma = 3, Crisp = 8, Hop = 4, Malt = 2, Fruity = 1, Sour = 0, ABV = 3, Roasty = 0, Sweetness = 1 },
+                 new FlavorProfile() { Id = 60, Color = 3, Aroma = 7, Crisp = 0, Hop = 3, Malt = 8, Fruity = 8, Sour = 0, ABV = 10, Roasty = 4, Sweetness = 7 }
                  ) ;
             });
         }
