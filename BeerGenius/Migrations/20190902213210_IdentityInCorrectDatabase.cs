9ed5a9c535b17994392b0d1e736c3aa19c940af5
MadisonMott <Madison.Mott@ymail.com>
2019-09-02 20:02:32 -0400

    Attempting to migrate identity to BeerGeniusDbContext

---
+++ BeerGenius/Migrations/20190902213210_IdentityInCorrectDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeerGenius.Migrations
+{
+    public partial class IdentityInCorrectDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
